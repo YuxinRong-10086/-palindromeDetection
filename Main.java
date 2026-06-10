c08741de284fd0e56b746d6591d52ed7ae04b2b7
YuxinRong-10086 <evelynrong11@gmail.com>
2026-06-10 09:03:28 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
 public class Main{
-    public static void Main(String args[]){
+    public static void main(String args[]){
         int count = 0;
         Scanner input = new Scanner(System.in);
         String inputText = input.nextLine();
-        for(int i = 0; i < inputText.length(); i++ ) {
-            if (character.isLetter(inputText.charAt(i))) {
-                count++
-            }
-        }
-            int mid = count/2;
-        for(int index = 0; index <=mid; index++ )
-            if (char.IsLetterOrDigit(inputText[index])) {
-                Console.WriteLine(text[index]);
-            }
-        for(int index = inputText.length; index >mid; index--){
-            if (char.IsLetterOrDigit(inputText[index])){
-                Console.WriteLine(text[index]);
-            }
 
+        if(first.euqal(last)) {
+            System.out.println("It's a Palindrome!");
         }
+        else{
+            System.out.println("Not a Palindrome!");
+        }
+
+        String one = first(inputText);
+        String two = last(inputText);
+        for(int i = 0; i < inputText.length(); i++ ) {
+            if (inputText.isLetter(inputText.charAt(i))) {
+                inputText.charAt(i) = inputText.charAt(i).toLowerCase();
+                count++;
+            }
+        }
+        public static String first(string x) {
+            int mid = count / 2;
+            for (int index = 0; index <= mid; index++) {
+                if (char.IsLetterOrDigit(x[index])) {
+                    Console.WriteLine(text[index]);
+                }
+            }
+        }
+        public static String last(String x) {
+            for (int index = x.length; index > mid; index--) {
+                if (char.IsLetterOrDigit(x[index])) {
+                    Console.WriteLine(text[index]);
+                }
+            }
+        }
+
     }
 }
+
